6b5f291a22198cdc807c57e6855ee428d6693a42
issas <sherif.issa@pega.com>
2020-08-17 09:32:02 -0500

    config.js

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ function base64ToBlob(base64) {
   for (let i = 0; i < len; ++i) {
     bytes[i] = binaryString.charCodeAt(i);
   }
-
   return new Blob([bytes], { type: 'application/pdf' });
 }
 
@@ -19,8 +18,6 @@ window.addEventListener('viewerLoaded', function () {
 
 window.addEventListener('documentLoaded', () => {
   const annotManager = docViewer.getAnnotationManager();
-  debugger;
-
   annotManager.exportAnnotCommand()
   .then(xfdfStringCmd => {
     annotManager.on("annotationChanged", (annotations, action) => {
@@ -30,10 +27,8 @@ window.addEventListener('documentLoaded', () => {
             }
             , '*'
           );
-          debugger                                        
         });
   });
-
 
   const rectangle = new Annotations.RectangleAnnotation();
   rectangle.PageNumber = 2;
@@ -46,43 +41,20 @@ window.addEventListener('documentLoaded', () => {
 });
 
 function receiveMessage(event) {
-  console.log("===>>>>>FROM receiveMessage...");
-
-
   if (event.isTrusted && typeof event.data === 'object') {
-
-
     switch (event.data.type) {
       case 'OPEN_DOCUMENT_URL':
-
-  debugger
-  
-  /*
-  fetch("https://ltgcrazyhorse.com/prweb?pyActivity=GetBase64Attachment&DocumentId=PEGASOCIAL-DOCUMENT%20DOC-4001", {
-    credentials: "include",
-    mode: 'no-cors',
-  }).then(response => {
-    debugger;
-
-
-  }).catch(err => {
-    debugger;
-    console.log(err);
-  })
-  */
-  
-
-        const { url } = event.data;
         const { fileName, data } = JSON.parse(event.target.readerControl.getCustomData());
         debugger;
-
-        /* event.target.readerControl.loadDocument(url); */
-
-        event.target.readerControl.loadDocument( base64ToBlob(data), { filename: 'myfile.pdf' } );
+        event.target.readerControl.loadDocument( base64ToBlob(data), { filename } );
         break;
       case 'CLOSE_DOCUMENT':
         event.target.readerControl.closeDocument();
         break;
+      case 'ANNOTATION_PUBLISHED':
+        debugger;
+        console.log(event);
+        break;        
       default:
         break;
     }
